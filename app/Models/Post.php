87fd9920cdef898aee94bfe49d4d7daa85f3c9fb
MarioLuna <mario@vivri.com>
2017-06-04 11:53:53 -0500

    Utilización de atributos en los modelos

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
@@ -11,4 +12,15 @@ class Post extends Model
     protected $fillable = [
     	'content',
     ];
+
+    public function getCreatedAtAttribute($parametro1){
+    	return $parametro1;
+    	return Carbon::now()->diffForHumans($parametro1);
+    }
+
+    public function getContentAttribute($parametro1){
+    	return strtoupper($parametro1);
+    }
+
+
 }
